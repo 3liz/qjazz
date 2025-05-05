bfbfe6d5c5df36bb1fb8aff0440759d24aa551e0
David Marteau <dmarteau@3liz.com>
2025-05-05 13:37:20 +0200

    [rpc] Add inspect lib

---
+++ qjazz-server/python/qjazz_rpc/inspect.py
@@ -0,0 +1,78 @@
+""" Utilities for inspecting cache and catalog objects
+
+    Used a debugging tools inside containers
+"""
+from functools import cached_property
+from pathlib import Path
+from typing import (
+    Optional,
+    Protocol,
+    cast,
+)
+
+from qjazz_ogc import Catalog, CatalogItem
+
+from qjazz_cache.prelude import CacheManager
+from qjazz_contrib.core import logger
+from qjazz_contrib.core.qgis import current_qgis_application, init_qgis_application
+
+from .main import WORKER_SECTION, WorkerConfig
+
+
+class ConfigProto(Protocol):
+    worker: WorkerConfig
+
+
+def load_config(path: Optional[Path|str], **kwds) -> WorkerConfig:
+    """
+    """
+    from qjazz_contrib.core import config
+
+    confservice = config.ConfBuilder()
+    confservice.add_section(WORKER_SECTION, WorkerConfig)
+
+    conf = config.read_config_toml(Path(path), **kwds) if path else {}
+    return cast(ConfigProto, confservice.validate(conf)).worker
+
+
+def init_cache(conf: WorkerConfig) -> CacheManager:
+
+    CacheManager.initialize_handlers(conf.qgis.projects)
+
+    cm = CacheManager(conf.qgis.projects)
+    cm.register_as_service()
+
+    return cm
+
+
+class Setup:
+    def __init__(self, path: Optional[Path|str], **kwds):
+        self.conf = load_config(path, **kwds)
+        self.catalog = Catalog()
+
+    @property
+    def qapp(self):
+        return current_qgis_application()
+
+    @cached_property
+    def cache(self):
+        return init_cache(self.conf)
+
+    def update_catalog(self, prefix: Optional[str] = None) -> Catalog:
+        cat = self.catalog
+        cat.update(self.cache, not self.conf.qgis.load_project_on_request, prefix=prefix)
+        return cat
+
+    def get_catalog_item(self, resource: str) -> Optional[CatalogItem]:
+        return self.catalog.get_and_update(self.cache, resource)
+
+
+def init(
+    config_path: Optional[Path],
+    log_level: logger.LogLevel = logger.LogLevel.DEBUG,
+    **kwds,
+) -> Setup:
+
+    logger.setup_log_handler(log_level)
+    init_qgis_application()
+    return Setup(config_path)
