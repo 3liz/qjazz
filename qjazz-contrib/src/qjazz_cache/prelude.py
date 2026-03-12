8d2c2e906fcd85110c7172b070d40f6733b817bd
Your Name <you@example.com>
2026-03-12 22:40:42 +0000

    Update cache prelude

--- qjazz-contrib/src/qjazz_cache/prelude.py
+++ qjazz-contrib/src/qjazz_cache/prelude.py
@@ -1,9 +1,5 @@
 # Re-export
-from .errors import (  # noqa F401
-    ResourceNotAllowed,
-)
-
-from .cachemanager import (  # noqa F401
+from .cachemanager import (
     CacheEntry,
     CacheManager,
     CheckoutStatus,
@@ -12,8 +8,28 @@ from .cachemanager import (  # noqa F401
     ProtocolHandler,
     Url,
 )
-
-from .resources import (  # noqa F401
+from .common import (
+    ProjectLoader,
+)
+from .errors import (
+    ResourceNotAllowed,
+)
+from .resources import (
     RemoteResources,
     ResourceStore,
 )
+
+__all__ = (
+    "CacheEntry",
+    "CacheManager",
+    "CheckoutStatus",
+    "ProjectLoader",
+    "ProjectMetadata",
+    "ProjectsConfig",
+    "ProtocolHandler",
+    "RemoteResources",
+    "ResourceNotAllowed",
+    "ResourceStore",
+    "Url",
+)
+
