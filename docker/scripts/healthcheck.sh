1926a451bcdcd139d3ea34695129f13b548256bc
David Marteau <dmarteau@3liz.com>
2026-04-16 17:22:45 +0200

    [docker] build attestations]

--- docker/scripts/healthcheck.sh
+++ docker/scripts/healthcheck.sh
@@ -5,7 +5,7 @@
 
 if [ -f /tmp/.qjazz-rpc-running ]; then
     # QJazz RPC is running
-    qjazz-rpc-client ping
+    qjazz-rpc-client healthcheck --set-error
 elif [ -f /tmp/.qjazz-map-running ]; then
     # QJazz map server is running 
     [ `curl -I -A "Healtcheck" -o /dev/null -s -w '%{http_code}' 'http://localhost:9080/ping'` == 200 ]
